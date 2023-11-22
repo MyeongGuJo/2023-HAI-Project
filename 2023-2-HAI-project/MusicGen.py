fccbbc9ea0a30cfc5ae2c9141ab70c74cd938d68
조명구 <102133534+MyeongGuJo@users.noreply.github.com>
2023-11-22 18:07:52 +0900

    My_Music_Gen

--- 2023-2-HAI-project/MusicGen.py
+++ 2023-2-HAI-project/MusicGen.py
@@ -4,67 +4,67 @@ import torchaudio.transforms as transforms
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
 
 class MusicGen:
-	def __init__(self, sample_rate):
-		self.processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
-		self.model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
-		self.prompt = []
-		self.audio_values = []
-		self.path = 'sample_audio/test.mp3'
-		self.sample_rate=sample_rate  # 24000
+    def __init__(self, sample_rate):
+        self.processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+        self.model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+        self.prompt = ''
+        self.audio_values = []
+        self.path = 'sample_audio/test0.mp3'
+        self.sample_rate=sample_rate
 
-	def generate_music(self, prompt1, prompt2):
-		self.prompt = [prompt1, prompt2]
+    def generate_music(self, prompt):
+        self.prompt = prompt
 
-		inputs = self.processor(
-			text=self.prompt,
-			padding=True,
-			return_tensors="pt",
-		)
+        inputs = self.processor(
+            text=self.prompt,
+            padding=True,
+            return_tensors="pt",
+        )
 		
-		self.audio_values = self.model.generate(
-			**inputs,
-   			do_sample=True,
-			guidance_scale=3,
-			max_new_tokens=256
-		)
+        self.audio_values = self.model.generate(
+            **inputs,
+            do_sample=True,
+            guidance_scale=3,
+            max_new_tokens=256
+        )
 
-		self.audio_values = self.audio_values.reshape(2, -1)
+        torchaudio.save(
+            uri=self.path,
+            src=self.audio_values[0],
+            sample_rate=self.sample_rate,
+            format='mp3'
+        )
 
-		torchaudio.save(
-			uri=self.path,
-			src=self.audio_values,
-			sample_rate=self.sample_rate,
-			format='mp3'
-		)
+    def regenerate_music(self, i):
+        '''
+        print(f'previous audio shape: {self.audio_values.shape}')
 
-	def regenerate_music(self):
-		print(f'previous audio shape: {self.audio_values.shape}')
-  
-		# mono_audio = torchaudio.transforms.DownmixMono(self.audio_values)
-		mono_audio = torch.mean(self.audio_values, dim=0, keepdim=True)
-		mono_audio = mono_audio.reshape(-1)
-		
-		print(f'mono audio shape: {mono_audio.shape}')
+        # mono_audio = torchaudio.transforms.DownmixMono(self.audio_values)
+        mono_audio = torch.mean(self.audio_values, dim=0, keepdim=True)
+        mono_audio = mono_audio.reshape(-1)
+        
+        print(f'mono audio shape: {mono_audio.shape}')
+        '''
+        audio_values = torchaudio.load(f'sample_audio/test{i-1}.mp3')[0]
 
-		inputs = self.processor(
-			audio=mono_audio,
-			text=self.prompt,
-			padding=True,
-			return_tensors="pt",
-		)
+        inputs = self.processor(
+            audio=audio_values[0],
+            text=self.prompt,
+            padding=True,
+            return_tensors="pt",
+        )
 
-		self.audio_values = self.model.generate(
-			**inputs,
-   			do_sample=True,
-			guidance_scale=3,
-			max_new_tokens=256
-		)
+        self.audio_values = self.model.generate(
+            **inputs,
+            do_sample=True,
+            guidance_scale=3,
+            max_new_tokens=256
+        )
 
-		self.audio_values = self.audio_values.reshape(2, -1)
-
-		torchaudio.save(
-			uri=self.path,
-			src=self.audio_values,
-			sample_rate=self.sample_rate,
-			format='mp3'
-		)
+        path = f'sample_audio/test{i}.mp3'
+        torchaudio.save(
+            uri=path,
+            src=self.audio_values[0],
+            sample_rate=self.sample_rate,
+            format='mp3'
+        )
--- 2023-2-HAI-project/app.py
+++ 2023-2-HAI-project/app.py
@@ -2,30 +2,32 @@ import streamlit as st
 import numpy as np
 from MusicGen import MusicGen
 
-sample_rate = 24000  # {sample_rate} samples per second
+sample_rate = 32000  # {sample_rate} samples per second
 music_gen = MusicGen(sample_rate)
 
 st.header('Music Generator')
 
-prompt1 = st.text_area('1st Prompt',
-                      placeholder='Write text to describe the music you want.'
-					)
-
-prompt2 = st.text_area('2nd Prompt',
-                      placeholder='Write text to describe the music you want.'
-					)
+prompt = st.text_area('Prompt',
+                placeholder='Write text to describe the music you want.'
+            )
 
 st.button("Reset", type="primary")
+
 if st.button('Submit'):
-	music_gen.generate_music(prompt1, prompt2)
-	audio_file = open('sample_audio/test.mp3', 'rb')
-	audio_bytes = audio_file.read()
+    music_gen.generate_music(prompt)
+    audio_file = open('sample_audio/test0.mp3', 'rb')
+    audio_bytes = audio_file.read()
 
-	st.audio(audio_bytes, format='audio/mp3')
+    st.audio(audio_bytes, format='audio/mp3')
 
-	while st.button("stop", type="primary") is False:
-		music_gen.regenerate_music()
-		audio_file = open('sample_audio/test.mp3', 'rb')
-		audio_bytes = audio_file.read()
+    for i in range(1, 11):
+        if st.button("stop", key=i, type="primary") is False:
+            music_gen.regenerate_music(i)
+            path = f'sample_audio/test{i}.mp3'
+            audio_file = open(path, 'rb')
+            audio_bytes = audio_file.read()
+            
+            st.audio(audio_bytes, format='audio/mp3')
+
 else:
     st.write('Music Player Here')
